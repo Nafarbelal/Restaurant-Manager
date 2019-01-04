81f1e6a91f4273899c7ea0c1816f979a2c1a9335
Josmael <JoJosmael@users.noreply.github.com>
2019-01-04 13:42:24 +0100

    Commentaire test

--- src/kikisrestaurantmanager/Home.java
+++ src/kikisrestaurantmanager/Home.java
@@ -17,7 +17,9 @@ public class Home extends javax.swing.JFrame {
     public Home() {
         initComponents();
     }
-
+    
+    // Hello 
+    // testing commits
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
