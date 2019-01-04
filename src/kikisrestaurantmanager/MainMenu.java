7249629c4d9442287a32aa8b3401945f1bb1c258
Yassmina212 <kaoutar.aouladhaj@gmail.com>
2019-01-04 13:52:17 +0100

    commit 2

--- src/kikisrestaurantmanager/MainMenu.java
+++ src/kikisrestaurantmanager/MainMenu.java
@@ -22,7 +22,7 @@ public class MainMenu extends javax.swing.JFrame {
      */
     GestionRestau Gr = new GestionRestau();
     // fsdfsd
-    // test 1
+    // commit2
     // test2
     
     public MainMenu() {
