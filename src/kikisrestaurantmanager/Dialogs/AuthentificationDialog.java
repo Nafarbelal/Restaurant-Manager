8b5de8f0cc7b582c2ce01ec71c9eedb74b04a9d0
Ilyas <ilyaselbani@gmail.Com>
2019-02-01 00:47:22 +0100

    OrangeColors

--- src/kikisrestaurantmanager/Dialogs/AuthentificationDialog.java
+++ src/kikisrestaurantmanager/Dialogs/AuthentificationDialog.java
@@ -53,37 +53,32 @@ public class AuthentificationDialog extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        labelWrongPassword = new javax.swing.JLabel();
-        loginField = new javax.swing.JTextField();
-        passwordField = new javax.swing.JPasswordField();
-        jLabel8 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        passwordField = new javax.swing.JPasswordField();
+        labelWrongPassword = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setSize(new java.awt.Dimension(399, 399));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        labelWrongPassword.setFont(new java.awt.Font("Montserrat", 0, 18)); // NOI18N
-        labelWrongPassword.setForeground(new java.awt.Color(255, 99, 71));
-        labelWrongPassword.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        labelWrongPassword.setText("Login et mot de passe incorrects");
-        getContentPane().add(labelWrongPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(-4, 420, 400, 20));
+        jLabel4.setFont(new java.awt.Font("Source Sans Pro", 0, 22)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Connectez-vous");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 90, 400, -1));
 
-        loginField.setBackground(new java.awt.Color(242, 246, 249));
-        loginField.setFont(new java.awt.Font("Montserrat", 0, 15)); // NOI18N
-        loginField.setForeground(new java.awt.Color(54, 33, 89));
-        loginField.setBorder(null);
-        loginField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                loginFieldActionPerformed(evt);
-            }
-        });
-        getContentPane().add(loginField, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 282, 200, 30));
+        jLabel5.setFont(new java.awt.Font("Source Sans Pro", 0, 36)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("MANAGER MODE");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 40, 400, -1));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         passwordField.setBackground(new java.awt.Color(242, 246, 249));
         passwordField.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
@@ -94,7 +89,15 @@ public class AuthentificationDialog extends javax.swing.JDialog {
                 passwordFieldActionPerformed(evt);
             }
         });
-        getContentPane().add(passwordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 359, 200, 40));
+
+        labelWrongPassword.setFont(new java.awt.Font("Montserrat", 0, 18)); // NOI18N
+        labelWrongPassword.setForeground(new java.awt.Color(255, 99, 71));
+        labelWrongPassword.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelWrongPassword.setText("Mot de passe incorrecte");
+
+        jLabel2.setFont(new java.awt.Font("Montserrat", 0, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(54, 33, 89));
+        jLabel2.setText("Password :");
 
         jLabel8.setFont(new java.awt.Font("Source Sans Pro", 0, 36)); // NOI18N
         jLabel8.setForeground(new java.awt.Color(255, 255, 255));
@@ -110,44 +113,45 @@ public class AuthentificationDialog extends javax.swing.JDialog {
                 jLabel8KeyPressed(evt);
             }
         });
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 460, 400, 70));
-
-        jLabel4.setFont(new java.awt.Font("Source Sans Pro", 0, 22)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setText("Connectez-vous");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 90, 400, -1));
-
-        jLabel5.setFont(new java.awt.Font("Source Sans Pro", 0, 36)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setText("MANAGER MODE");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 40, 400, -1));
-
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel1.setFont(new java.awt.Font("Montserrat", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(54, 33, 89));
-        jLabel1.setText("Login :");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, -1, -1));
-
-        jLabel2.setFont(new java.awt.Font("Montserrat", 0, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(54, 33, 89));
-        jLabel2.setText("Password :");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 325, 130, 30));
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/loginFixed.png"))); // NOI18N
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 530));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 399, 530));
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(80, 80, 80)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(100, 100, 100)
+                .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(labelWrongPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(240, 240, 240)
+                .addComponent(jLabel2)
+                .addGap(10, 10, 10)
+                .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(labelWrongPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(380, 380, 380)
+                .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 530, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 399, 450));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jLabel8MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel8MousePressed
         System.out.println("Se Connecter Clicked");
-        String username = loginField.getText();
         String password = passwordField.getText();
         Employee emp = dbManager.getLoginInfos();
         if (emp == null) {
@@ -164,10 +168,6 @@ public class AuthentificationDialog extends javax.swing.JDialog {
             }
         }
     }//GEN-LAST:event_jLabel8MousePressed
-
-    private void loginFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginFieldActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_loginFieldActionPerformed
 
     private void passwordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordFieldActionPerformed
         // TODO add your handling code here:
@@ -219,7 +219,6 @@ public class AuthentificationDialog extends javax.swing.JDialog {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -227,7 +226,6 @@ public class AuthentificationDialog extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel labelWrongPassword;
-    private javax.swing.JTextField loginField;
     private javax.swing.JPasswordField passwordField;
     // End of variables declaration//GEN-END:variables
 }
